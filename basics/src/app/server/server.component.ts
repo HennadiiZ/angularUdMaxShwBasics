a71ee00b2e89dfedaed8238ba05a88231ad96188
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-10-19 14:37:49 +0300

    18. Creating a New Component

---
+++ basics/src/app/server/server.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.scss']
+})
+
+export class ServerComponent implements OnInit {
+
+  ngOnInit(): void {}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-server',
+//   templateUrl: './server.component.html',
+//   styleUrls: ['./server.component.scss']
+// })
+// export class ServerComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
