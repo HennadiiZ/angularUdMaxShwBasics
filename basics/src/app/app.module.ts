4bbbb719403a7edeeb7136a02067d1183ff62078
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-10-19 16:55:00 +0300

    19. Understanding the Role of AppModule and Component Declaration, 20. Using Custom Components, 21. Creating Components with the CLI & Nesting Components, 22. Working with Component Templates

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
@@ -23,3 +25,8 @@ export class AppModule {}
 
 
 // 18. Creating a New Component
+
+// 19. Understanding the Role of AppModule and Component Declaration
+// 20. Using Custom Components
+// 21. Creating Components with the CLI & Nesting Components
+// 22. Working with Component Templates
