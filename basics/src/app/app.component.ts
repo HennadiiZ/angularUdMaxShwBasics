e1005177126ca3c543fadb40b85ca6cfdf17ff5d
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-10-26 19:11:30 +0300

    127. Setting up and Loading Routes + 128. Navigating with Router Links

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private router: Router) {}
+
+  goToHomePage(): void {
+    console.log('Home page');
+    this.router.navigate(['']);
+  }
+
+  goToUsersPage(): void {
+    console.log('Users page');
+    this.router.navigate(['users']);
+  }
+
+  goToServersPage(): void {
+    console.log('Servers page');
+    this.router.navigate(['servers']);
+  }
 }
--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -8,7 +8,14 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }, // localhost:4200/
+  { path: 'servers', component: ServersComponent }, // localhost:4200/servers
+  { path: 'users', component: UsersComponent } // localhost:4200/users
+];
 
 @NgModule({
   declarations: [
@@ -23,7 +30,19 @@ import { UsersComponent } from './users/users.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+// 125. Why do we need a Router?
+// 126. Understanding the Example Project
+
+
+// 127. Setting up and Loading Routes
+// 128. Navigating with Router Links
